fbec73d33e49a301909b1ebcf5e912ff52e57ae8
Gustavo Jeronimo <gustavo.jeronimo@hotmail.com>
2021-08-12 20:42:22 -0300

    Configura o uso do Javascript do Bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -27,12 +27,32 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-          "postcss-loader"
-        ],
+        test: /\.(scss)$/,
+        use: [{
+          // inject CSS to page
+          loader: 'style-loader'
+        }, {
+          // translates CSS into CommonJS modules
+          loader: 'css-loader'
+        }, {
+          // Run postcss actions
+          loader: 'postcss-loader',
+          options: {
+            // `postcssOptions` is needed for postcss 8.x;
+            // if you use postcss 7.x skip the key
+            postcssOptions: {
+              // postcss plugins, can be exported to postcss.config.js
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        }, {
+          // compiles Sass to CSS
+          loader: 'sass-loader'
+        }]
       },
       {
         test: /\.html$/i,
